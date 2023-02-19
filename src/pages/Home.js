5eda2ea1f5b905d1e0d136849be56cc5f8473a3c
racketysky <igor.star.ie@gmail.com>
2023-02-19 18:21:33 -0300

    [refactor] updating home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
 import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import { Header } from '../components/Header'
+import { useEffect, useState } from "react";
 
 export function Home(props) {
+
+    const [user, setUser] = useState();
+    // const [isLogged, setIsLogged] = useState();
+
+    useEffect(() => {
+        if (window.localStorage !== null) {
+            const data = localStorage.getItem('user')
+            setUser(data)
+            // const isLogged = localStorage.getItem('isLogged')
+            // if (isLogged !== null || isLogged !== undefined) {
+            //     window.location.href = `/`
+            // }
+        }
+    }, [])
+
+    function redirect(e, type) {
+        e.preventDefault();
+        window.location.href = `/chat?type=${type}`
+    }
+
+    const text = `Olá ${user}, me chamo bot, estou aqui para te ajudar a praticar questões de Algoritmos. Por favor selecione abaixo para qual prova você gostaria de praticar.`
+    // console.log(typeof(isLogged))
     return (
         <Container fluid className="h-100">
             <Row>
@@ -14,16 +37,17 @@ export function Home(props) {
                         <Col xs={12} sm={12} md={12} lg={12} xl={12} className="mt-3 bot-text">
                             <Image src='bot.png' width={100} className="mt-2 mb-2"></Image>
                             <p>
-                                <span className="type" style={{'--n':157}}>
-                                    Olá Usuário, me chamo bot, estou aqui para te ajudar a praticar questões de Algoritmos. Por favor selecione abaixo para qual prova você gostaria de praticar.
+                                <span className="type" style={{ '--n': text.length }}>
+                                    {text}
                                 </span>
                             </p>
+
                         </Col>
                         <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <Button className="btn-question" title="As questões da primeira prova abordam comando de atribuição, entrada de dados, saída de dados e condicional.">Primeira Prova</Button>
+                            <Button className="btn-question" onClick={(e) => redirect(e, 1)} title="As questões da primeira prova abordam comando de atribuição, entrada de dados, saída de dados e condicional.">Primeira Prova</Button>
                         </Col>
                         <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <Button className="btn-question" title="As questões da segunda prova abordam laços de repetição, vetores e matrizes.">Segunda Prova</Button>
+                            <Button className="btn-question" onClick={(e) => redirect(e, 2)} title="As questões da segunda prova abordam laços de repetição, vetores e matrizes.">Segunda Prova</Button>
                         </Col>
                     </Row>
                 </Col>
