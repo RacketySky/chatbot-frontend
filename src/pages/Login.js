33344ac2341fc11d5ad13ac959a93c6397fbaad8
racketysky <igor.star.ie@gmail.com>
2023-02-19 18:21:05 -0300

    [refactor] updating login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import { FormControl, TextField } from '@mui/material';
+import Axios from 'axios';
+import { Loading } from '../components/Loading'
+// import { redirect } from "react-router-dom";
+
 
 export function Login(props) {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const auth = async (e) => {
+        e.preventDefault();
+        document.getElementById('login-btn').setAttribute('disabled', '');
+        document.getElementById('loading').removeAttribute('hidden');
+        await Axios.post('https://chatbot-backend-hb2o.onrender.com/auth', {
+            email,
+            password
+        })
+            .then((res) => {
+                document.getElementById('loading').setAttribute('hidden', '');
+                console.log('success', res.data)
+                localStorage.setItem('user', res.data.user);
+                localStorage.setItem('token', res.data.token);
+                localStorage.setItem('isLogged', true);
+                window.location.href = '/home'
+                // return redirect("/home");
+            })
+            .catch(() => console.log('failed'))
+    }
+
 
     return (
         <Container fluid className='background-login'>
@@ -13,7 +38,7 @@ export function Login(props) {
                 <Col className='card-login' xs={12} sm={12} md={6} lg={4} xl={4}>
                     <Image className="mt-2 mb-2" src='bot.png' width={55} />
                     <form>
-                        <FormControl>
+                        <FormControl >
                             <TextField
                                 className=""
                                 id="email"
@@ -33,14 +58,15 @@ export function Login(props) {
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
-                            <Button type='subimit'>Login</Button>
+                            <Button type="submit" id="login-btn" onClick={auth}>Login</Button>
                         </FormControl>
                     </form>
-                    <div className='line'/>
+                    <div className='line' />
                     <Button className="singin-btn" type='button' href="/singin">Cadastrar</Button>
                 </Col>
                 <Col xs={0} sm={0} md={2} lg={4} xl={4} />
             </Row>
+            <Loading />
         </Container>
     )
 }
