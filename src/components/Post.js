e99f5b4123c9eeb4541787cfc030d7ec897231dd
Igor Estrela <igor.star.ie@gmail.com>
2024-05-15 20:02:11 -0300

    [refactor] changing bot image

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,7 @@ export const Post = (props) => {
         <Container fluid className="mt-3 mb-3">
             <Row>
                 <Col xs={2} sm={2} md={2} lg={2} xl={2} className='img-bot'>
-                    <Image src='bot.png' width={45} />
+                    <Image src='cosmoBot.png' width={45} />
                 </Col>
 
                 <Col xs={10} sm={10} md={10} lg={10} xl={10} className='bot-response'>
