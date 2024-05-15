bee740581cc923696d5862caacbd3170b2fe816a
Igor Estrela <igor.star.ie@gmail.com>
2024-05-15 20:03:43 -0300

    [refactor] improve homepage to better responsivity

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ export function Home(props) {
         window.location.href = `/chat?type=${type}`
     }
 
-    const text = `Olá ${user}, me chamo bot, estou aqui para te ajudar a praticar questões de Algoritmos. Por favor selecione abaixo para qual prova você gostaria de praticar.`
+    const text = `Olá ${user}, me chamo Cosmobot, estou aqui para te ajudar a praticar questões de Algoritmos. Por favor selecione abaixo para qual prova você gostaria de praticar.`
     // console.log(typeof(isLogged))
     return (
         <Container fluid className="h-100">
@@ -31,11 +31,12 @@ export function Home(props) {
                 <Header></Header>
             </Row>
             <Row className="text-center opn align-items-center">
-                <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
-                <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                <Col xs={0} sm={0} md={1} lg={3} xl={3} className="opnSides"></Col>
+                <Col xs={12} sm={12} md={10} lg={6} xl={6} className="opnM">
                     <Row>
+
                         <Col xs={12} sm={12} md={12} lg={12} xl={12} className="mt-3 bot-text">
-                            <Image src='bot.png' width={100} className="mt-2 mb-2"></Image>
+                            <Image src='cosmoBot.png' width={100} className="mt-2 mb-2"></Image>
                             <p>
                                 <span className="type" style={{ '--n': text.length }}>
                                     {text}
@@ -43,15 +44,16 @@ export function Home(props) {
                             </p>
 
                         </Col>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+
+                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                             <Button className="btn-question" onClick={(e) => redirect(e, 1)} title="As questões da primeira prova abordam comando de atribuição, entrada de dados, saída de dados e condicional.">Primeira Prova</Button>
                         </Col>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                             <Button className="btn-question" onClick={(e) => redirect(e, 2)} title="As questões da segunda prova abordam laços de repetição, vetores e matrizes.">Segunda Prova</Button>
                         </Col>
                     </Row>
                 </Col>
-                <Col xs={0} sm={0} md={3} lg={3} xl={3}></Col>
+                <Col xs={0} sm={0} md={1} lg={3} xl={3} className="opnSides"></Col>
             </Row>
         </Container>
     )
